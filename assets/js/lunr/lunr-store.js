e46539bfbee94e54a4e80a5502ee89780d3e3ac2
Still34 <Still34@users.noreply.github.com>
2022-11-18 05:42:16 +0000

    jekyll build from Action 82088bd0a5862af3b7ca3672cc585aa2baafd562

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,127 @@
+var store = [{
+        "title": "AIS3 2019 Ransomware Sample Analysis",
+        "excerpt":"Dynamic analysis Dynamic analysis is used here first because it is already actively running on victim’s machine, so we might as well start from there. Discovering malicious executable Launch Process Hacker on the victim VM; an anomaly named 5D85C2C17D.exe can be found in the process list. The process is launched...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "/writeup/2019/07/31/ais3-2019-ransomware-sample-analysis/",
+        "teaser": null
+      },{
+        "title": "Capture the Nitro Writeup",
+        "excerpt":"So the story goes like this: a few weeks ago my friend took notice that I was playing CTF. He got curious and decided to make a little CTF challenge specifically for me. The prize? One month Nitro Classic on Discord. I was like sure, sounds like fun. For the...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "/writeup/2020/06/23/capture-the-nitro/",
+        "teaser": null
+      },{
+        "title": "AIS3 2020 Pre-exam Writeup (中文)",
+        "excerpt":"前言 小弟中文不好，如果有中文不通順的地方請見諒 &lt;(＿　＿)&gt; 這次比我去年第一次打的時候有經驗多了，去年差不多100名左右，這次爬到了 43 名。很可惜的，有幾題我都是戳中邊或者是花太多時間在部分題目導致其他幾題沒有多餘時間看完。比如像是 Clara，Clara 那題我很早就看到有問題的封包甚至是 Key Exchange 中的 Key，但是只能說經驗不足，一開始沒意識到我所看到的字串部分是 Key，而加密方式是純粹 XOR。我是在比賽結束後沒多久才發現這回事的。非常可惜因為 Clara 那題 500 分。但整體而言我還是對這次 AIS3 很滿意也覺得又學到了不少，如 Rose 那題，我在之前其實沒有意識到 Python 在執行時會 Compile 成 Bytecode，也趁機學會了這點。 Web 🐿 Squirrel (100 pt.) 這題一打開會看到一堆到處在跑的松鼠，一開始完全不知道在幹嘛，但那些松鼠上寫的用戶名非常似曾相識… 這些不是常見的Linux使用者帳號嗎？ 估計是從 /etc/passwd 翻出來的，咱們來看看這個網頁的原始碼。 ❯ curl https://squirrel.ais3.org/ &lt;!DOCTYPE html&gt; &lt;head&gt; &lt;title&gt;🐿️🐿️🐿️🐿️🐿️&lt;/title&gt; &lt;meta charset=\"utf-8\"&gt; &lt;link rel=\"stylesheet\" href=\"css/style.css\"&gt; &lt;/head&gt;...","categories": ["writeup"],
+        "tags": ["zh-tw"],
+        "url": "/writeup/2020/06/28/ais3-preexam-zh-2020/",
+        "teaser": null
+      },{
+        "title": "The Current State of the Internet and its Absurdity",
+        "excerpt":"Today I was sorting through the extensions I’ve been using over the past few months. There’s the standard bunch like uBlock Origin, Dark Reader, Stylish, and whatnot. You know, something that everyone might have in their browser. What about the rest? The rest are mostly “privacy-focused” extensions: Decentraleyes, Privacy Badger,...","categories": ["rambling"],
+        "tags": ["en-us"],
+        "url": "/rambling/2020/06/29/internet-and-its-absurdity/",
+        "teaser": null
+      },{
+        "title": "Strays and Morality",
+        "excerpt":"2AM. Awaken by the itching of my overly sensitive skin. Tried to fall back to sleep, alas, all efforts were in vain. Stomach growled. 3AM, defeated, I biked to the nearest McDonald’s to grab a quick meal; the meal was nothing more than just a cheap chicken sandwich and a...","categories": ["rambling"],
+        "tags": ["philosophy","en-us"],
+        "url": "/rambling/2020/06/29/strays-and-morality/",
+        "teaser": null
+      },{
+        "title": "Struggling with Gender Dysphoria",
+        "excerpt":"Junior high was the darkest time of my life. I was bullied heavily at school because of my personal hygiene (or lack thereof) and various family-related issues. My family had always fought over one thing or another. I had an abusive pedo girlfriend who was much older than I was....","categories": ["rambling"],
+        "tags": ["mental-health","en-us"],
+        "url": "/rambling/2020/07/10/struggling-with-gender-dysphoria/",
+        "teaser": null
+      },{
+        "title": "rgbCTF 2020 Writeup - Part 1",
+        "excerpt":"Prologue I was not planning on participating any CTF this week, but I figured I’d check the CTFTime schedule few days ago. That was when I saw a CTF named “rgbCTF” was coming up within a few days. I was like “screw it, let’s go, why not?” My interest peaked...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "/writeup/rgbctf-2020-part-1",
+        "teaser": null
+      },{
+        "title": "rgbCTF 2020 Writeup - Part 2",
+        "excerpt":"Prologue This is second part of the rgbCTF writeup, and contains information about challenges solved by me, Emzi. I strongly suggest starting with the first part of this writeup. This was my first CTF event, however not the first set of challenges I solved. As I’m new, the methods outlined...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "/writeup/rgbctf-2020-part-2",
+        "teaser": null
+      },{
+        "title": "A Closer Look at CAPA",
+        "excerpt":"What is it While looking around Fireeye’s website for the latest news on their Flare-on CTF, I found that they had just recently released a new tool for threat researchers, dubbed capa. I thought it’d be a perfect opportunity to further expand ever-growing collection of Windows Security Tools. So what...","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "/infosec/2020/07/19/closer-look-at-capa/",
+        "teaser": null
+      },{
+        "title": "Windows Security Tools",
+        "excerpt":"Reverse Engineering Use multiple tools for comparison when decompiling! Every tool creates different results. Always use multiple decompilers if you encounter weird results. Generic Binaries Ghidra ✅ Pricing: Free ✅ Codebase: Open-source 📝 Notes: Made by NSA Binary Ninja ⚠ Pricing: Paid ($149 USD/$4,500 NTD) ⚠ Codebase: Proprietary 📝 Notes:...","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "/windows-security-tools",
+        "teaser": null
+      },{
+        "title": "Humanity and Sentimental Value of Songs",
+        "excerpt":"When I fell in love with Avicii’s work right around the time Level was released, I once swore that progressive house would be my only favorite genre of music and would listen to nothing else. I had a special relationship with the genre after all; it was the genre that...","categories": ["personal"],
+        "tags": ["philosophy","en-us"],
+        "url": "/personal/2020/08/03/humanity-songs/",
+        "teaser": null
+      },{
+        "title": "Threat Spotlight - DLL Hijacking (and .NET)",
+        "excerpt":"Welcome to a new series I’m calling Threat Spotlight! In this series, we will focus on common vulnerabilities or potential risk factors that developers or security researchers should consider when securing their systems. In this article, we’ll be focusing on the way Windows handles DLL loading, how threat actors may...","categories": ["threat-spotlight"],
+        "tags": ["en-us","dotnet","guide"],
+        "url": "/threat-spotlight/2020/09/16/dll-hijacking/",
+        "teaser": null
+      },{
+        "title": "The HITCON 2020 Experience",
+        "excerpt":"⚠ Disclaimer This trip was made by possible by the funding provided by the HITCON 2020 Student Program. While select students are required to write a publicly accessible review of the event, HITCON was NOT given any copy approval or an early preview. I try to present my articles as...","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "/infosec/2020/09/28/hitcon-2020/",
+        "teaser": null
+      },{
+        "title": "\\\"Hated by Life Itself (命に嫌われている)\\\"",
+        "excerpt":"Several weeks ago, my friend on Discord sent me a link to a song. This friend sends me YouTube music links a lot. I don’t remember why but at first I didn’t click on the link. It wasn’t until they sent another version of the same song later that I...","categories": ["personal"],
+        "tags": ["philosophy","en-us"],
+        "url": "/personal/2020/10/11/hated-by-life-itself/",
+        "teaser": null
+      },{
+        "title": "A New Chapter in Life",
+        "excerpt":"Applying for Work In search for a new job for my future (and for my wallet), I applied for a little company in Taiwan called TeamT5 back in late September. TeamT5 provides threat analysis and incident response services for enterprises, which happens to align with my interest of information security....","categories": ["personal"],
+        "tags": ["en-us"],
+        "url": "/personal/2020/10/25/new-chapter-of-life/",
+        "teaser": null
+      },{
+        "title": "The Mysterious SYSTEM_THREAD_EXCEPTION_NOT_HANDLED and Hyper-V",
+        "excerpt":"TL;DR If the BSoD happened after enabling Hyper-V-related features, and if you are on an AMD platform, disable AMD PSS in the BIOS. Disabling Memory Integrity supposedly works too, but NOT RECOMMENDED for security reasons. The Story My weekend could have been a lot smoother this week. Here’s the story:...","categories": ["misc"],
+        "tags": ["en-us"],
+        "url": "/misc/2020/11/16/mystery-bsod-system-thread-exception-not-handled-amd/",
+        "teaser": null
+      },{
+        "title": "How I Extracted an Unreal Engine Game's WWise Audio",
+        "excerpt":"⚠ Disclaimer The following post has the game’s name, ID, and encryption key redacted. I’ve been playing a game that I love, and the best part of it is its wicked soundtrack. Buuuut, the publisher hasn’t released the official soundtrack for it, and I just couldn’t wait for its release....","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "/infosec/2021/03/01/obtaining-unreal-pak-decryption-key/",
+        "teaser": null
+      },{
+        "title": "Lockdown, Taiwan, 2021",
+        "excerpt":"Throughout 2020, Taiwan had done an amazing job at keeping COVID out of the country. Daily cases were low and practically nonexistent. Unfortunately, all of it came crashing down some time around May 2021. It all started with pilots being infected and improper quarantining of the few travelers that were...","categories": ["personal"],
+        "tags": ["en-us"],
+        "url": "/personal/2021/07/12/lockdown-taiwan-2021/",
+        "teaser": null
+      },{
+        "title": "Facebook, Monopoly, and Barinsta",
+        "excerpt":"Facebook, Monopoly, and Barinsta Last week of October 2021 marks the week that my second Instagram account got suspended. My first and my main Instagram account that was created since 2013 was suspended on the last week of September 2021 as well. Over what? We’ll get into that later, but...","categories": ["personal"],
+        "tags": ["en-us"],
+        "url": "/personal/2021/10/29/barinsta-and-facebook/",
+        "teaser": null
+      },{
+        "title": "Threat Spotlight - Domain Fronting",
+        "excerpt":"Domain fronting is a common technique that is sometimes used by threat actors to disguise their traffic as the real deal. Essentially, what it is is communicate with legitimate-looking domains when reality, the traffic is being pointed to threat actor’s C2 stations. A common example would be using legitimate or...","categories": ["threat-spotlight"],
+        "tags": ["en-us","guide"],
+        "url": "/threat-spotlight/2021/11/13/domain-fronting-fastly/",
+        "teaser": null
+      },{
+        "title": "IDA Trickeries #1: Using Saved Desktop Across IDAs",
+        "excerpt":"Welcome to a new series I’m calling IDA Trickeries! In this series, I’ll be looking at weird IDA quirks or not-so-well-known IDA tips that I’ve learned over the years. Today’s topic: desktop layouts - specifically, the weirdness you may encounter when you try to use saved layouts across different IDA...","categories": ["ida-tips"],
+        "tags": ["en-us"],
+        "url": "/ida-tips/2022/01/22/saved-desktops-across-32bit-64bit/",
+        "teaser": null
+      }]
